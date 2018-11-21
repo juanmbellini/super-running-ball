03e19ef20d508f89988b549a23f7bea292f44077
Facundo Gonzalez Fernandez <fgonzalez@itba.edu.ar>
2018-11-21 18:37:23 -0300

    Add pause and import UI controller from previous project

---
+++ Assets/Scripts/Pause.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+
+public class Pause : MonoBehaviour {
+    /// <summary>
+    /// Game object for the pause message.
+    /// </summary>
+    [SerializeField] private GameObject pausePanel;
+
+    /// <summary>
+    /// Flag indicating the game is paused.
+    /// </summary>
+    public static bool GamePaused;
+
+
+    private void Start() {
+        pausePanel.SetActive(false);
+    }
+
+    private void Update() {
+        if (!Input.GetKeyDown(KeyCode.Escape)) {
+            return;
+        }
+        if (GamePaused) {
+            ContinueGame();
+        }
+        else {
+            PauseGame();
+        }
+    }
+
+    private void FixedUpdate() {
+    }
+
+    /// <summary>
+    /// Pauses the game.
+    /// </summary>
+    private void PauseGame() {
+        // Disable scripts that still work while timescale is set to 0
+        pausePanel.SetActive(true);
+        Time.timeScale = 0;
+        GamePaused = true;
+    }
+
+    /// <summary>
+    /// Unpauses the game.
+    /// </summary>
+    private void ContinueGame() {
+        // Enable the scripts again
+        pausePanel.SetActive(false);
+        Time.timeScale = 1;
+        GamePaused = false;
+    }
+}
