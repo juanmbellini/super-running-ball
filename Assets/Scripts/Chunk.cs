3d820152146942b6c498637a1e02c30f6b96ff9f
Juan Marcos Bellini <juanmbellini@gmail.com>
2018-11-21 22:56:21 -0300

    Improve level generation

--- Assets/Scripts/Chunk.cs
+++ Assets/Scripts/Chunk.cs
@@ -1,15 +1,22 @@
-﻿using Boo.Lang.Runtime;
+﻿using System;
+using Boo.Lang.Runtime;
 using UnityEngine;
 
 /// <summary>
 /// The logic stuff of a chunk.
 /// </summary>
-[System.Serializable]
+[Serializable]
 public class Chunk : MonoBehaviour {
     /// <summary>
     /// The chunk's length.
     /// </summary>
     [SerializeField] private float _chunkLength;
+
+    /// <summary>
+    /// The chunks difficulty.
+    /// </summary>
+    [SerializeField] private int _difficulty;
+
 
     /// <summary>
     /// Chunk's length getter.
@@ -18,10 +25,20 @@ public class Chunk : MonoBehaviour {
         get { return _chunkLength; }
     }
 
+    /// <summary>
+    /// The chunks difficulty. getter.
+    /// </summary>
+    public int Difficulty {
+        get { return _difficulty; }
+    }
+
     // Use this for initialization
     private void Start() {
         if (_chunkLength <= 0) {
             throw new RuntimeException("Chunk length must be positive.");
+        }
+        if (_difficulty < 0) {
+            throw new RuntimeException("Chunk difficulty cannot be negative.");
         }
     }
 
--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 /// <summary>
@@ -25,6 +26,41 @@ public class LevelManager : MonoBehaviour {
     [SerializeField] private Vector3 _playerStartingPosition;
 
     /// <summary>
+    /// Position at which the level starts building.
+    /// </summary>
+    [SerializeField] private Vector3 _levelStartingPosition;
+
+//    private int _startingAmount;
+
+    /// <summary>
+    /// The amount of chunks added to the level at once.
+    /// </summary>
+    [SerializeField] private int _buildBatchSize;
+
+    /// <summary>
+    /// The min. margin that must exists between the player's position and the end of the level.
+    /// </summary>
+    [SerializeField] private int _creationMarginSize;
+
+
+    // ================================================================================================================
+    // Internal state
+    // ================================================================================================================
+
+    /// <summary>
+    /// The player that will walk the level.
+    /// </summary>
+    public Player Player { get; set; }
+
+    private int _amountOfCreatedChunks;
+    private float _nextStartingPosition;
+
+    /// <summary>
+    /// A Dictionary containing the chunks according to the defficulty.
+    /// </summary>
+    private Dictionary<int, List<Chunk>> _chunksByDifficulty;
+
+    /// <summary>
     /// Player starting position getter.
     /// </summary>
     public Vector3 PlayerStartingPosition {
@@ -33,25 +69,75 @@ public class LevelManager : MonoBehaviour {
 
     // Use this for initialization
     private void Start() {
-        BuildLevel(0, _chunksPrefabs.Count);
+        _nextStartingPosition = _levelStartingPosition.x;
+        _amountOfCreatedChunks = 0;
+        _chunksByDifficulty = SeparateByDifficulty();
+
+//        BuildLevel(_chunksPrefabs.Count);
     }
 
     // Update is called once per frame
     private void Update() {
+        CheckLevelExpansion();
     }
 
+    private void CheckLevelExpansion() {
+        if (ShouldExpand()) {
+            ExpandLevel();
+        }
+    }
+
+    private bool ShouldExpand() {
+        return _nextStartingPosition - Player.transform.position.x < _creationMarginLength;
+    }
+
+
     /// <summary>
-    /// Creates some level.
+    /// Expands the level.
     /// </summary>
-    /// <param name="from">The first inclusive chunk number to be built (i.e the next to be added to the level).</param>
-    /// <param name="to">The last exclusive chunk number to be built.</param>
-    private void BuildLevel(int from, int to) {
-        // TODO: make it random
-        for (var i = 0; i < to - from; i++) {
-            var chunk = Instantiate(_chunksPrefabs[i]);
-            chunk.name = "Chunk" + i;
-            chunk.transform.position = new Vector3(i * chunk.ChunkLength, 0, 0);
-            // TODO: save it in a manager for reuse?
+    private void ExpandLevel() {
+        BuildLevel(_buildBatchSize);
+    }
+
+
+    /// <summary>
+    /// Creates a Dictionary containing the chunks prefabs grouped by their difficulty.
+    /// </summary>
+    /// <returns>A Dictionary containing the chunks prefabs grouped by their difficulty.</returns>
+    private Dictionary<int, List<Chunk>> SeparateByDifficulty() {
+        return _chunksPrefabs.ToList()
+            .GroupBy(c => c.Difficulty)
+            .ToDictionary(c => c.Key, c => c.ToList());
+    }
+
+
+    private readonly List<Chunk> _createdChunks = new List<Chunk>();
+
+    private float _creationMarginLength;
+
+    private void BuildLevel(int amount) {
+        var total = _amountOfCreatedChunks + amount;
+        while (_amountOfCreatedChunks < total) {
+            // Calculate a random chunk TODO: improve according to difficulty
+            var selectedChunk = Random.Range(0, _chunksPrefabs.Count);
+            var chunk = Instantiate(_chunksPrefabs[selectedChunk]);
+            chunk.name = "Chunk" + _amountOfCreatedChunks;
+
+            // The chunk must start in the next starting position 'x' value
+            var chunkLength = chunk.ChunkLength;
+            var chunkX = _nextStartingPosition + chunkLength / 2;
+            chunk.transform.position = new Vector3(chunkX, _levelStartingPosition.y, _levelStartingPosition.z);
+
+            // Update internal variables
+            _nextStartingPosition += chunkLength;
+            _amountOfCreatedChunks++;
+            _createdChunks.Add(chunk);
+        }
+
+        // Calculate the margin in 'x' at which the level should be expanded.
+        _creationMarginLength = 0;
+        for (var i = _createdChunks.Count - 1; i >= _createdChunks.Count - _creationMarginSize; i--) {
+            _creationMarginLength += _createdChunks[i].ChunkLength;
         }
     }
 }
